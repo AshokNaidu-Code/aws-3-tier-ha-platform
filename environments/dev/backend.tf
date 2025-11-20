b482f732167415e01d26478f5d5906ef35fe36f9
Ashok22555 <ashoknallam03@gmail.com>
2025-11-20 12:33:08 +0530

    Deployment file modified

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "aws-3tier-ha-tfstate-712111072557"
     key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "aws-3tier-ha-tfstate-lock"
   }
